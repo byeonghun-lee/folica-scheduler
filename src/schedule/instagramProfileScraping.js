837734c452b0063331a0b19ff7ae1efd8905ec1a
byeonghun <byeonghun08@gmail.com>
2024-03-02 01:57:40 +0900

    인스타 프로필 스크래핑 코드 추가

--- src/schedule/instagramProfileScraping.js
+++ src/schedule/instagramProfileScraping.js
@@ -1,6 +1,8 @@
 const { connect, disconnect } = require("../db");
 const { SnsProfile } = require("../models/snsProfile");
 const puppeteer = require("puppeteer");
+const axios = require("axios");
+const FormData = require("form-data");
 
 module.exports.instagramProfileScraping = async () => {
     console.log("START INSTAGRAM PROFILE SCRAPING.");
@@ -13,13 +15,12 @@ module.exports.instagramProfileScraping = async () => {
             name: { $exists: false },
             desc: { $exists: false },
             imageUrl: { $exists: false },
-        })
-            .limit(1)
-            .sort({ _id: -1 });
+        }).sort({ _id: -1 });
         console.log("targetProfileList:", targetProfileList);
-
+        
         const browser = await puppeteer.launch({ headless: false });
         const page = await browser.newPage();
+        await new Promise((resolve) => setTimeout(resolve, 30000));
 
         const profileImgPath = "section header > div img";
         const profileNamePath =
@@ -37,25 +38,48 @@ module.exports.instagramProfileScraping = async () => {
             console.log("페이지 대기 완료");
 
             const profileImg = await page.evaluate(
-                (profileImgPath) => document.querySelector(profileImgPath).src,
+                (profileImgPath) => document.querySelector(profileImgPath)?.src,
                 profileImgPath
             );
             console.log("profileImg:", profileImg);
             const name = await page.evaluate(
                 (profileNamePath) =>
-                    document.querySelector(profileNamePath).innerText,
+                    document.querySelector(profileNamePath)?.innerText,
                 profileNamePath
             );
             console.log("name:", name);
             const desc = await page.evaluate(
                 (profileDescPath) =>
-                    document.querySelector(profileDescPath).innerText,
+                    document.querySelector(profileDescPath)?.innerText,
                 profileDescPath
             );
             console.log("desc:", desc);
-            // 업데이트 해주고.
-            // 이미지 cloudflare에 넣어주고
+
+            const form = new FormData();
+            form.append("url", profileImg);
+            form.append("metadata", JSON.stringify({ sns: "instagram" }));
+
+            const uploadImage = await axios.post(
+                `https://api.cloudflare.com/client/v4/accounts/${process.env.CLOUD_FLARE_ACCOUNT_ID}/images/v1`,
+                form,
+                {
+                    headers: {
+                        Authorization: `Bearer ${process.env.CLOUD_FLARE_TOKEN}`,
+                    },
+                }
+            );
+
+            console.log("uploadImage:", uploadImage.data);
+
+            targetInstagram.name = name;
+            targetInstagram.desc = desc;
+            targetInstagram.updatedAt = new Date();
+            targetInstagram.imageUrl = uploadImage.data.result.variants[0];
+            await targetInstagram.save();
+
+            await new Promise((resolve) => setTimeout(resolve, 2000));
         }
+        console.log("완료");
     } catch (error) {
         console.log("INSTAGRAM PROFILE ERROR:", error);
     }
