aedab8ab84fd8b399698f6d80446d3ac7939e0c3
byeonghun <byeonghun08@gmail.com>
2024-04-14 23:32:29 +0900

    블로그 키워드 생성 후 관련 DB 데이터 생성

--- src/models/dailyKeywordScraping.js
+++ src/models/dailyKeywordScraping.js
@@ -6,8 +6,8 @@ dayjs.locale("ko");
 
 const DailyKeywordScrapingSchema = new Schema({
     keyword: { type: mongoose.Types.ObjectId, required: true, ref: "Keyword" },
-    screenShotUrl: { type: Boolean, required: true },
-    textContent: { type: String }, // 스크린샷안에 있는 html을 json으로 구조화? 또는 html 그대로 넣기
+    screenShotUrl: { type: String, required: true },
+    textContent: { type: Array },
     createdAt: { type: Date, default: () => dayjs().toDate() },
 });
 
--- src/schedule/naverBlogKeywordScraping.js
+++ src/schedule/naverBlogKeywordScraping.js
@@ -3,9 +3,24 @@ const axios = require("axios");
 const fs = require("fs");
 const path = require("path");
 const FormData = require("form-data");
+const dayjs = require("dayjs");
+require("dayjs/locale/ko");
+dayjs.locale("ko");
+
+const { connect, disconnect } = require("../db");
+const { Keyword } = require("../models/keyword");
+const { DailyKeywordScraping } = require("../models/dailyKeywordScraping");
+const { KeywordRelation } = require("../models/keywordRelations");
+const { keywordScrapingLog } = require("../models/keywordScrapingLog");
 
 module.exports.naverBlogKeywordScraping = async () => {
     console.log("START NAVER BLOG KEYWORD SCRAPING.");
+    const folderName = `blogKeyword_${dayjs().format("YYYY_MM_DD_HH_mm")}`;
+    fs.mkdirSync(folderName, {
+        recursive: true,
+    });
+
+    await connect();
 
     // 키워드를 가져와
     // 키워드에 대한 dailyScraping을 만들어줘
@@ -19,75 +34,110 @@ module.exports.naverBlogKeywordScraping = async () => {
     const blogContentDesc = ".detail_box > .dsc_area";
 
     try {
+        const keywordList = await Keyword.find({}).limit(1).lean();
+
         const browser = await puppeteer.launch({ headless: false });
         const page = await browser.newPage();
 
-        const keyword = "수원구치소";
+        for (let index = 0; index < keywordList.length; index++) {
+            const keywordItem = keywordList[index];
 
-        await page.goto(
-            `https://search.naver.com/search.naver?ssc=tab.blog.all&sm=tab_jum&query=${keyword}`
-        );
-        await page.waitForSelector(blogListWrapperPath);
+            console.log("keywordItem:", keywordItem);
 
-        const blogDataList = await page.evaluate(
-            ({
-                blogListPath,
-                blogMainUrl,
-                blogContentTitle,
-                blogContentDesc,
-            }) => {
-                const blogList = Array.from(
-                    document.querySelectorAll(blogListPath)
-                );
-                console.log("blogList:", blogList);
+            await page.goto(
+                `https://search.naver.com/search.naver?ssc=tab.blog.all&sm=tab_jum&query=${keywordItem.name}`
+            );
 
-                return blogList
-                    .filter((blog) => !!blog.querySelector(blogMainUrl))
-                    .map((blog) => {
-                        const userBlogUrl =
-                            blog.querySelector(blogMainUrl).href;
-                        const userBlogContentUrl =
-                            blog.querySelector(blogContentTitle).href;
+            await page.waitForSelector(blogListWrapperPath);
 
-                        const userBlogContentTitle =
-                            blog.querySelector(blogContentTitle).innerText;
-                        const userBlogContentDesc =
-                            blog.querySelector(blogContentDesc).innerText;
+            const blogDataList = await page.evaluate(
+                ({
+                    blogListPath,
+                    blogMainUrl,
+                    blogContentTitle,
+                    blogContentDesc,
+                }) => {
+                    const blogList = Array.from(
+                        document.querySelectorAll(blogListPath)
+                    );
+                    console.log("blogList:", blogList);
 
-                        return {
-                            userBlogUrl,
-                            userBlogContentUrl,
-                            userBlogContentTitle,
-                            userBlogContentDesc,
-                        };
-                    });
-            },
-            { blogListPath, blogMainUrl, blogContentTitle, blogContentDesc }
-        );
+                    return blogList
+                        .filter((blog) => !!blog.querySelector(blogMainUrl))
+                        .map((blog) => {
+                            const userBlogUrl =
+                                blog.querySelector(blogMainUrl).href;
+                            const userBlogContentUrl =
+                                blog.querySelector(blogContentTitle).href;
 
-        console.log("blogDataList:", blogDataList);
+                            const userBlogContentTitle =
+                                blog.querySelector(blogContentTitle).innerText;
+                            const userBlogContentDesc =
+                                blog.querySelector(blogContentDesc).innerText;
 
-        await page.screenshot({
-            path: "fullpage.png",
-            fullPage: true,
-        });
-
-        const form = new FormData();
-        const image = fs.readFileSync(
-            path.join(__dirname, "..", "..", "fullpage.png")
-        );
-        form.append("file", image, "fullpage.png");
-
-        const uploadImage = await axios.post(
-            `https://api.cloudflare.com/client/v4/accounts/${process.env.CLOUD_FLARE_ACCOUNT_ID}/images/v1`,
-            form,
-            {
-                headers: {
-                    Authorization: `Bearer ${process.env.CLOUD_FLARE_TOKEN}`,
+                            return {
+                                userBlogUrl,
+                                userBlogContentUrl,
+                                userBlogContentTitle,
+                                userBlogContentDesc,
+                            };
+                        });
                 },
-            }
-        );
-        console.log("uploadImage:", uploadImage?.data);
+                { blogListPath, blogMainUrl, blogContentTitle, blogContentDesc }
+            );
+
+            console.log("blogDataList:", blogDataList);
+
+            const screenshotFileName = `${keywordItem.name}_${dayjs().format(
+                "YYYY_MM_DD_HH_mm"
+            )}.png`;
+
+            await page.screenshot({
+                path: `${folderName}/${screenshotFileName}`,
+                fullPage: true,
+            });
+
+            const form = new FormData();
+            const image = fs.readFileSync(
+                path.join(__dirname, "..", "..", folderName, screenshotFileName)
+            );
+
+            form.append("file", image, screenshotFileName);
+
+            const uploadImage = await axios.post(
+                `https://api.cloudflare.com/client/v4/accounts/${process.env.CLOUD_FLARE_ACCOUNT_ID}/images/v1`,
+                form,
+                {
+                    headers: {
+                        Authorization: `Bearer ${process.env.CLOUD_FLARE_TOKEN}`,
+                    },
+                }
+            );
+            console.log("uploadImage:", uploadImage?.data);
+
+            await DailyKeywordScraping.create({
+                keyword: keywordItem._id,
+                screenShotUrl: uploadImage?.data?.result?.variants[0],
+                textContent: blogDataList,
+            });
+
+            const keywordRelationList = await KeywordRelation.find({
+                keyword: keywordItem._id,
+                isDeleted: false,
+            }).lean();
+            console.log("keywordRelationList:", keywordRelationList);
+
+            await keywordScrapingLog.create(
+                keywordRelationList.map((relationItem) => ({
+                    keywordRelation: relationItem._id,
+                    userId: relationItem.userId,
+                    action: "addScrapingData",
+                }))
+            );
+        }
+
+        fs.rmSync(folderName, { recursive: true, force: true });
+        console.log("완료");
     } catch (error) {
         console.log("Error:", error);
     }
