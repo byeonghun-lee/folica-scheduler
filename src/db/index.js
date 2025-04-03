ff7dc6f57115e9d67aa78448d76b7fe747b20c51
byeonghun <byeonghun08@gmail.com>
2025-04-03 12:01:02 +0900

    여러 함수 실행시 디비 연결 끊어지는 문제 수정

--- src/db/index.js
+++ src/db/index.js
@@ -1,38 +1,54 @@
 const mongoose = require("mongoose");
 
+let isConnected = false;
+let connectionCount = 0;
+
 module.exports.connect = async () => {
-    if (mongoose.connection.readyState === 1) {
-        return;
+    if (!isConnected) {
+        const safeOn = (event, handler) => {
+            if (mongoose.connection.listenerCount(event) === 0) {
+                mongoose.connection.on(event, handler);
+            }
+        };
+
+        safeOn("error", (error) => {
+            console.log(`DB connect error:`, error);
+        });
+
+        safeOn("disconnected", () => {
+            console.log("DB disconnected.");
+            isConnected = false;
+        });
+
+        safeOn("close", () => {
+            console.log("DB close.");
+            isConnected = false;
+        });
+
+        safeOn("connected", () => {
+            console.log("DB connected!");
+        });
+
+        await mongoose.connect(process.env.DB_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+
+        isConnected = true;
     }
 
-    const safeOn = (event, handler) => {
-        if (mongoose.connection.listenerCount(event) === 0) {
-            mongoose.connection.on(event, handler);
-        }
-    };
-
-    safeOn("error", (error) => {
-        console.log(`DB connect error:`, error);
-    });
-
-    safeOn("disconnected", () => {
-        console.log("DB disconnected.");
-    });
-
-    safeOn("close", () => {
-        console.log("DB close.");
-    });
-
-    safeOn("connected", () => {
-        console.log("DB connected!");
-    });
-
-    await mongoose.connect(process.env.DB_URL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    });
+    connectionCount++;
+    console.log("Connect connectionCount:", connectionCount);
 };
 
 module.exports.disconnect = async () => {
-    await mongoose.disconnect();
+    connectionCount--;
+    console.log("Disconnect connectionCount:", connectionCount);
+
+    if (connectionCount <= 0 && isConnected) {
+        await mongoose.disconnect();
+        isConnected = false;
+        connectionCount = 0;
+        console.log("Disconnected from DB");
+    }
 };
